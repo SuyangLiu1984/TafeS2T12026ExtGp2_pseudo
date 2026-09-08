79e3b798773cb9e8985be0489e34f162f0f2dde3
001178581 <suyang.liu@student.tafesa.edu.au>
2026-09-09 08:44:36 +0930

    Add Main Menu

---
+++ UniversalCalculator/MainMenu_pseudo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator
+{
+	public sealed partial class MainMenu_pseudo : Page
+	{
+		public MainMenu_pseudo()
+		{
+			InitializeComponent();
+		}
+
+		private void pageLoaded(object sender, RoutedEventArgs e)
+		{
+			ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(256, 384));
+
+			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+			ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+			titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.White;
+		}
+	}
+}
